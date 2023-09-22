2fc377a4148c68447b818fca60f2108b06edda68
Caleb M Carroll <144181450+TheFa1seKing@users.noreply.github.com>
2023-09-22 13:02:03 -0700

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 function palindrome_check(){  //funtion to check if the string is a palindrome
     var input =document.getElementById("input").value;  //get the value from user input
-    var reg = /[\W_]/g;  // replace any non word character to an empty string (ie, remove it)
+    var reg = /[^a-zA-Z0-9]/g;  // replace any non word character to an empty string (ie, remove it)
     var string = input.toLowerCase().replace(reg, "");  //converts the string to lowercase letters.
     var reversed = string.split("").reverse().join("");  //splits the string into characters, reverses it, then makes it back into a string
     if (reversed === string){  //checks if the string is a palindrome then produces a result
